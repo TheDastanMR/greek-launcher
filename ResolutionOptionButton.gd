51fac46e9adb83171f67e6d574f49035d387b7dc
TheDastanMR <42654984+TheDastanMR@users.noreply.github.com>
2023-09-01 14:35:37 +0200

    First push for FileAccess

--- ResolutionOptionButton.gd
+++ ResolutionOptionButton.gd
@@ -1,5 +1,7 @@
 extends OptionButton
 
+var default = "Default="
+
 var Resolutions: Dictionary = {"1024x768":Vector2(1024,768),
 								"1152x864":Vector2(1152,864),
 								"1280x720":Vector2(1280,720),
@@ -10,15 +12,36 @@ var Resolutions: Dictionary = {"1024x768":Vector2(1024,768),
 								"1680x1050":Vector2(1680,1050),
 								"1920x1080":Vector2(1920,1080)}
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("Resolutions started")
+	addResolution()
+	fileAccess()
 
+func addResolution():
+	print("Founded resolutions:")
+	for r in Resolutions:
+		add_item(r)
+		print(r)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+func fileAccess():
+	var file = FileAccess.open("0Settings.ini", FileAccess.READ_WRITE)
+	var lines = []
+	var test_lang = "Spanish"
+	var str_lines
+	while file.get_position()<file.get_length():
+		lines.append(file.get_line())
+		str_lines =  " ".join(lines)
+		
+	
+	#print(lines[1])
+	#file.store_string(str_lines)
+	#lines[1] = str(default)+str(test_lang)
+	#print(lines[1])
+	var x = 0
+	for i in lines:
+		#print(i," ",x)
+		print(str_lines[x])
+		x += 1
+		
 func _on_item_selected(index):
 	pass # Replace with function body.
